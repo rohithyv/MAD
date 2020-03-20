bc0c7f31706c01c76924b11e1d8d2fb9b21999f1
rohithyv <53818270+rohithyv@users.noreply.github.com>
2020-03-20 10:18:52 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,82 +1,58 @@
-package com.example.program4;
+package com.example.program2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-
-import android.graphics.Bitmap;
-
-import android.graphics.Canvas;
-
-import android.graphics.Color;
-
-import android.graphics.Paint;
-
-import android.graphics.drawable.BitmapDrawable;
-
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-import android.widget.ImageView;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends Activity
 
-{
+    EditText e1,e2;
+    Button bt;
+    Spinner s;
 
+
+    String [] dept_array={"CSE","ECE","IT","Mech","Civil"};
+    String name,reg,dept;
     @Override
-
-    public void onCreate(Bundle savedInstanceState)
-
-    {
-
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-//Creating a Bitmap
-
-        Bitmap bg = Bitmap.createBitmap(720, 1280, Bitmap.Config.ARGB_8888);
-
-//Setting the Bitmap as background for the ImageView
-
-        ImageView i = (ImageView) findViewById(R.id.imageView);
-
-        i.setBackgroundDrawable(new BitmapDrawable(bg));
-
-//Creating the Canvas Object
-
-        Canvas canvas = new Canvas(bg);
-
-//Creating the Paint Object and set its color & TextSize
-
-        Paint paint = new Paint();
 
 
+        e1= findViewById(R.id.editText);
+        e2= findViewById(R.id.editText2);
+        bt=  findViewById(R.id.button);
+        s=  findViewById(R.id.spinner);
 
-        paint.setTextSize(50);
+        ArrayAdapter adapter= new ArrayAdapter(MainActivity.this,android.R.layout.simple_spinner_item,dept_array);
+        s.setAdapter(adapter);
 
-//To draw a Rectangle
-        paint.setColor(Color.RED);
-        canvas.drawText("Rectangle", 420, 150, paint);
 
-        canvas.drawRect(400, 200, 650, 700, paint);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-//To draw a Circle
-        paint.setColor(Color.YELLOW);
-        canvas.drawText("Circle", 120, 150, paint);
+                name=e1.getText().toString();
+                reg=e2.getText().toString();
+                dept=s.getSelectedItem().toString();
 
-        canvas.drawCircle(200, 350, 150, paint);
+                Intent i = new Intent(MainActivity.this, com.example.program2.SecondActivity.class);
+                i.putExtra("name_key", name);
+                i.putExtra("reg_key",reg);
+                i.putExtra("dept_key", dept);
+                startActivity(i);
 
-//To draw a Square
-        paint.setColor(Color.GREEN);
-        canvas.drawText("Square", 120, 800, paint);
+            }
 
-        canvas.drawRect(50, 850, 350, 1150, paint);
-
-//To draw a Line
-        paint.setColor(Color.BLUE);
-        canvas.drawText("Line", 480, 800, paint);
-
-        canvas.drawLine(520, 850, 520, 1150, paint);
+        });
 
     }
 
