96d7020bcdf5242b336241aaa34b37e70be1f6a1
rohithyv <53818270+rohithyv@users.noreply.github.com>
2020-03-20 11:57:55 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,59 +1,66 @@
-package com.example.program2;
+package com.example.program1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.content.Intent;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-
+import android.widget.TextView;
+import android.graphics.Color;
 public class MainActivity extends AppCompatActivity {
-
-
-    EditText e1,e2;
-    Button bt;
-    Spinner s;
-
-
-    String [] dept_array={"CSE","ECE","IT","Mech","Civil"};
-    String name,reg,dept;
+    int ch=1;
+    float font=30;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-        e1= findViewById(R.id.editText);
-        e2= findViewById(R.id.editText2);
-        bt=  findViewById(R.id.button);
-        s=  findViewById(R.id.spinner);
-
-        ArrayAdapter adapter= new ArrayAdapter(MainActivity.this,android.R.layout.simple_spinner_item,dept_array);
-        s.setAdapter(adapter);
-
-
-        bt.setOnClickListener(new View.OnClickListener() {
+        final TextView t = (TextView) findViewById(R.id.textView);
+        Button b1= (Button) findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                t.setTextSize(font);
+                font = font + 5;
+                if(font == 50)
+                    font = 30; } });
 
-                name=e1.getText().toString();
-                reg=e2.getText().toString();
-                dept=s.getSelectedItem().toString();
-
-                Intent i = new Intent(MainActivity.this, com.example.program2.SecondActivity.class);
-                i.putExtra("name_key", name);
-                i.putExtra("reg_key",reg);
-                i.putExtra("dept_key", dept);
-                startActivity(i);
-
+        Button b2= (Button) findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch(ch) {
+                    case 1:
+                        t.setTextColor(Color.RED);
+                        break;
+                    case 2:
+                        t.setTextColor(Color.GREEN);
+                        break;
+                    case 3:
+                        t.setTextColor(Color.BLUE);
+                        break;
+                    case 4:
+                        t.setTextColor(Color.CYAN);
+                        break;
+                    case 5:
+                        t.setTextColor(Color.YELLOW);
+                        break;
+                    case 6:
+                        t.setTextColor(Color.MAGENTA);
+                        break;
+                } ch++;
+                if(ch == 7)
+                    ch = 1;
+            }   });
+//        TextView t= (TextView) findViewById(R.id.textView);
+        Button b3= (Button) findViewById(R.id.button3); b3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //Toggle Text
+                if(t.getText()=="CSE")
+                    t.setText("ISE");
+                else t.setText("CSE");
             }
-
         });
 
     }
-
 }
